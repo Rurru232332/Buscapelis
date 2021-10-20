644de59eb57699e11acd51308ea87fc8905cc551
Rurru232332 <rurru232332@gmail.com>
2021-10-20 17:08:45 +0200

    Down with the sickness

--- src/buscapelis/Partida.java
+++ src/buscapelis/Partida.java
@@ -13,7 +13,6 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
- *
  * @author Alumno Tarde
  */
 public class Partida {
@@ -22,109 +21,72 @@ public class Partida {
     private int puntos;
     private String titulo;
     
-    public Partida (boolean nueva){
-        if (nueva){
-            
-            FileReader fr = null;
-            BufferedReader br = null;
-            
-            //Contador de líneas del archivo
-            
-            int contadorSaltos = 0;
-            try {
-                fr = new FileReader ("peliculas.txt");
-                
-                
-                char caracterLeido = (char)fr.read();
-                
-                while (caracterLeido != '?'){
-                    if (caracterLeido == '\r'){
-                        contadorSaltos ++;
-                        //System.out.println(contadorSaltos);
-                    }
-                    
-                    caracterLeido = (char)fr.read();
+    public Partida (String ruta){ //constructor para cargar una partida
+        
+    }
+    
+    public Partida (){ //Constructor para una partida nueva
+
+        BufferedReader br = null;
+
+        int contadorSaltos = 0;
+
+
+        //System.out.print(aleatorio + " ");
+        try {
+
+            //CONTADOR DE LÍNEAS
+            br = new BufferedReader(new FileReader("peliculas.txt"));
+
+            while (br.readLine() != null){
+                contadorSaltos ++;
+            }
+
+            //ELECCIÓN DE TÍTULO
+            int aleatorio = (int)(Math.random() * contadorSaltos);
+            //System.out.println(aleatorio);
+            br = new BufferedReader(new FileReader("peliculas.txt"));
+
+            titulo = br.readLine();
+            while (titulo != null && aleatorio!=0){
+                aleatorio --;
+                if (aleatorio == 0){
+                    break;
                 }
-            }
-            catch (FileNotFoundException ex) {
-                //Logger.getLogger(Partida.class.getName()).log(Level.SEVERE, null, ex);
-                System.err.println("Error al abrir fr");
-            }
-            catch (IOException ex) {
-                //Logger.getLogger(Partida.class.getName()).log(Level.SEVERE, null, ex);
-                System.err.println("Error al leer fichero fr");
-            }
-            finally{
-                if (fr != null){
-                    try {
-                        fr.close();
-                    }
-                    catch (IOException ex) {
-                        //Logger.getLogger(Partida.class.getName()).log(Level.SEVERE, null, ex);
-                        System.err.println("Error al cerrar fichero fr");
-                    }
-                }
-            }
-            
-            //Selección de título
-            
-            int aleatorio = (int)(Math.random() * contadorSaltos) -1;
-            //System.out.print(aleatorio + " ");
-            try {
-                
-                br = new BufferedReader(new FileReader("peliculas.txt"));
                 titulo = br.readLine();
-                while (titulo != null){
-                    aleatorio --;
-                    if (aleatorio == 0){
-                        break;
-                    }
-                    titulo = br.readLine();
+            }
+
+            aciertos = new boolean[titulo.length()];
+
+            for (int k = 0; k < aciertos.length; k++){
+                if (titulo.charAt(k) == ' ' || titulo.charAt(k) == ':' || titulo.charAt(k) == '-'){
+                    aciertos[k] = true;
                 }
-                if (titulo == null){
-                    /*
-                    Esto es para que si por alguna razón el texto se queda en null,
-                    se le asigne un título y el programa no crashee
-                    Este título no aparece en el archivo peliculas.txt para que tenga
-                    la misma probabilidad de aparecer que el resto
-                    */
-                    titulo = "LA MOSCA";
-                }
-                aciertos = new boolean[titulo.length()];
-                
-                for (int k = 0; k < aciertos.length; k++){
-                    if (titulo.charAt(k) == ' ' || titulo.charAt(k) == ':' || titulo.charAt(k) == '-'){
-                        aciertos[k] = true;
-                    }
-                    else{
-                        aciertos[k] = false;
-                    }
+                else{
+                    aciertos[k] = false;
                 }
             }
-            catch (FileNotFoundException ex) {
-                //Logger.getLogger(Partida.class.getName()).log(Level.SEVERE, null, ex);
-                System.err.println("Error al abrir fichero");
-            }
-            catch (IOException ex) {
-                //Logger.getLogger(Partida.class.getName()).log(Level.SEVERE, null, ex);
-                System.err.println("Error al leer fichero");
-            }
-            finally{
-                if (br != null){
-                    try {
-                        br.close();
-                    }
-                    catch (IOException ex) {
-                        //Logger.getLogger(Partida.class.getName()).log(Level.SEVERE, null, ex);
-                        System.err.println("Error al cerrar fichero");
-                    }
+        }
+        catch (FileNotFoundException ex) {
+            //Logger.getLogger(Partida.class.getName()).log(Level.SEVERE, null, ex);
+            System.err.println("Error al abrir fichero");
+        }
+        catch (IOException ex) {
+            //Logger.getLogger(Partida.class.getName()).log(Level.SEVERE, null, ex);
+            System.err.println("Error al leer fichero");
+        }
+        finally{
+            if (br != null){
+                try {
+                    br.close();
+                }
+                catch (IOException ex) {
+                    //Logger.getLogger(Partida.class.getName()).log(Level.SEVERE, null, ex);
+                    System.err.println("Error al cerrar fichero");
                 }
             }
         }
         
-        else{
-            //cargar partida
-        }
     }
     
     public boolean[] getAciertos(){
@@ -141,7 +103,9 @@ public class Partida {
     
     public static void main (String[] args){
         
-            Partida p = new Partida(true);
+        for (int k = 0; k<10000; k++){
+            Partida p = new Partida();
             System.out.println(p.getTitulo());
+        }
     }
 }
